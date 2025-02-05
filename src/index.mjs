08ead68c6f2e85f59de982e57e6eb31141ad3177
Frederick Amoako-Atta <amoakofredatta@gmail.com>
2025-02-05 20:56:23 +0000

    Removed .env as part of commits

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,11 @@ import express from 'express';
 import 'dotenv/config';
 import {port} from "./utils/constants.mjs";
 import appRouter from "./routes/index.mjs";
-import testDB from "./db/queries.mjs";
 
 const app = express();
 
 app.use(express.json());
 app.use(appRouter);
 
-testDB().then();
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
